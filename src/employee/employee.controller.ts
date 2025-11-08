02981b2752d30adb2a0efbd55b9d4c37af6b967f
Xaletch <kiril.kolesnikov0609@mail.ru>
2025-11-08 18:51:02 +0300

    employee docs

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -11,26 +11,57 @@ import {
   UseGuards,
 } from "@nestjs/common";
 import { EmployeeService } from "./employee.service";
-import { EmployeeDto } from "./dto/employee.dto";
+import { CreateEmployeeResponse, EmployeeDto } from "./dto/employee.dto";
 import { Ip } from "src/shared/decorators/ip.decorator";
 import { RegisterEmployeeDto } from "./dto/register.dto";
 import { AuthGuard } from "src/auth/guard/auth.guard";
 import { LoadUserGuard } from "src/user/guard/user.guard";
 import { ScopeGuard } from "src/access/guard/scope.guard";
 import { Scopes } from "src/access/decorator/scopes.decorator";
-import { ApiTags } from "@nestjs/swagger/dist/decorators";
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger/dist/decorators";
 import { LocationGuard } from "src/access/guard/location.guard";
-import { CheckInviteDto } from "./dto/check-invite.dto";
+import {
+  CheckInviteDto,
+  CheckInviteResponseDto,
+  CheckInviteResponseErrorDto,
+} from "./dto/check-invite.dto";
 import { CompanyGuard } from "src/access/guard/company.guard";
-import { EmployeeUpdateDto } from "./dto/employee-update.dto";
-import { EmployeeBlockedDto } from "./dto/blocked.dto";
+import {
+  EmployeeUpdateDto,
+  EmployeeUpdateResponseDto,
+} from "./dto/employee-update.dto";
+import {
+  EmployeeBlockedDto,
+  EmployeeBlockedResponseDto,
+} from "./dto/blocked.dto";
+import { NotFoundDto, UnAuthorizedDto } from "src/shared/dto/errors.dto";
+import { AuthResponseDto } from "src/auth/dto/auth-response.dto";
 
 @ApiTags("Сотрудники")
 @Controller()
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
-  @Post("admin/employee")
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Создание сотрудника для компании" })
+  @ApiBody({ type: EmployeeDto })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: "created",
+    type: CreateEmployeeResponse,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "unauthorized",
+    type: UnAuthorizedDto,
+  })
+  @Post("employee/invite")
   @UseGuards(AuthGuard, LoadUserGuard, ScopeGuard)
   @Scopes("employee:invite")
   @HttpCode(HttpStatus.CREATED)
@@ -39,18 +70,62 @@ export class EmployeeController {
     return this.employeeService.create(dto, companyId);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Проверка на валидность токена авторизации" })
+  @ApiBody({ type: CheckInviteDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "success",
+    type: CheckInviteResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "not found",
+    type: CheckInviteResponseErrorDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "unauthorized",
+    type: UnAuthorizedDto,
+  })
   @Post("check-invite")
   @HttpCode(HttpStatus.OK)
   checkInvite(@Body() dto: CheckInviteDto) {
     return this.employeeService.checkInvite(dto);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Регистрация сотрудника" })
+  @ApiBody({ type: RegisterEmployeeDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "success",
+    type: AuthResponseDto,
+  })
   @Post("employee/register")
   @HttpCode(HttpStatus.CREATED)
   register(@Body() dto: RegisterEmployeeDto, @Ip() userIp) {
     return this.employeeService.register(dto, userIp);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Редактировать сотрудника" })
+  @ApiBody({ type: EmployeeUpdateDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "success",
+    type: EmployeeUpdateResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "not found",
+    type: NotFoundDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "unauthorized",
+    type: UnAuthorizedDto,
+  })
   @Patch("employee/:user_id")
   @UseGuards(AuthGuard, LoadUserGuard, CompanyGuard, ScopeGuard)
   @Scopes("employee:update")
@@ -59,6 +134,24 @@ export class EmployeeController {
     return this.employeeService.update(dto, userId);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Заблокировать сотрудника для локации" })
+  @ApiBody({ type: EmployeeBlockedDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "success",
+    type: EmployeeBlockedResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "not found",
+    type: NotFoundDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "unauthorized",
+    type: UnAuthorizedDto,
+  })
   @Post("employee/blocked/:user_id/:location_id")
   @UseGuards(AuthGuard, LoadUserGuard, LocationGuard, ScopeGuard)
   @Scopes("employee:update")
@@ -71,6 +164,23 @@ export class EmployeeController {
     return this.employeeService.blocked(dto, userId, locationId);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Удалить сотрудника из локации" })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "success",
+    type: EmployeeUpdateResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: "not found",
+    type: NotFoundDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: "unauthorized",
+    type: UnAuthorizedDto,
+  })
   @Delete("employee/:user_id/:location_id")
   @UseGuards(AuthGuard, LoadUserGuard, LocationGuard, ScopeGuard)
   @Scopes("employee:delete")
